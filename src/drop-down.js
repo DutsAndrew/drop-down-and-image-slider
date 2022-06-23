3ebe42aad34bf4c755e88b68b7290d1dd9775388
Andrew Dutson <Dutsandrew@gmail.com>
2022-06-23 11:29:08 -0600

    finished dynamic effects for drop down menu template

--- src/drop-down.js
+++ src/drop-down.js
@@ -1,24 +1,27 @@
 const dropDownSvg = document.querySelector('#drop-down-menu-svg');
-dropDownSvg.addEventListener('click', dropDownMenu);
+  dropDownSvg.addEventListener('click', dropDownMenu);
 
 function dropDownMenu() {
-    console.log('hey');
-
-    // const dropDownMenu = document.querySelector('#drop-down-menu');
-    // const dropDownTopBar = document.querySelector('#drop-down-top-bar');
-    // const dropDownSvg = document.querySelector('#drop-down-menu-svg');
+  const dropDownTopBar = document.querySelector('#drop-down-top-bar');
+    dropDownTopBar.classList.toggle('clicked');
 
   const dropDownNav = document.querySelector('#drop-down-nav');
-    if (dropDownNav.classList.contains('drop-down-nav-closed')) {
-      dropDownNav.classList.remove('drop-down-nav-closed');
-      dropDownNav.classList.add('drop-down-nav-open');
-    } else if (dropDownNav.classList.contains('drop-down-nav-open')) {
-      dropDownNav.classList.add('drop-down-nav-closed');
-      dropDownNav.classList.remove('drop-down-nav-open');
-    }
+    dropDownNav.classList.toggle('drop-down-nav-open');
+    dropDownNav.classList.toggle('drop-down-collapsed');
     
-    const dropDownItemsContainer = document.querySelector('#drop-down-items-container');
-    const dropDownHome = document.querySelector('#drop-down-home');
-    const dropDownAbout = document.querySelector('#drop-down-about');
-    const dropDownContact = document.querySelector('#drop-down-contact');
+  const dropDownItemsContainer = document.querySelector('#drop-down-items-container');
+    dropDownItemsContainer.classList.toggle('drop-down-items-container-open');
+    dropDownItemsContainer.classList.toggle('drop-down-collapsed');
+
+  const dropDownHome = document.querySelector('#drop-down-home');
+    dropDownHome.classList.toggle('drop-down-home-open');
+    dropDownHome.classList.toggle('drop-down-collapsed');
+
+  const dropDownAbout = document.querySelector('#drop-down-about');
+    dropDownAbout.classList.toggle('drop-down-about-open');
+    dropDownAbout.classList.toggle('drop-down-collapsed');
+
+  const dropDownContact = document.querySelector('#drop-down-contact');
+    dropDownContact.classList.toggle('drop-down-contact-open');
+    dropDownContact.classList.toggle('drop-down-collapsed');
 }
